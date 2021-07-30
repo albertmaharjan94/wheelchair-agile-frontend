3b72450bbb595b7149f704b1ca4de3ae20441a0c
bakanatsume <chhantyalbipash@gmail.com>
2021-07-30 09:58:14 +0545

    vehicle details added

---
+++ pages/admin/vehicle.js
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from "react";
+import { MDBDataTableV5 } from "mdbreact";
+
+import Modal from "react-modal";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// reactstrap components
+import {
+  Badge,
+  Card,
+  CardHeader,
+  CardFooter,
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Media,
+  Pagination,
+  PaginationItem,
+  PaginationLink,
+  Progress,
+  Table,
+  Container,
+  Row,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+import AddVehicle from "./AddVehicle";
+
+// layout for this page
+import Admin from "layouts/Admin.js";
+// core components
+import UserHeader from "../../components/Headers/UserHeader";
+import { Button } from "react-bootstrap";
+
+function Vehicle() {
+  const [modalIsOpen, setModalIsOpen] = React.useState(false);
+
+  const setModalIsOpenToTrue = () => {
+    setModalIsOpen(true);
+  };
+  const setModalIsOpenToFalse = () => {
+    setModalIsOpen(false);
+  };
+
+  const [datatable, setDatatable] = React.useState({
+    columns: [
+      {
+        label: "Name",
+        field: "name",
+        width: 100,
+        attributes: {
+          "aria-controls": "DataTable",
+          "aria-label": "Name",
+        },
+      },
+      {
+        label: "Position",
+        field: "position",
+        width: 100,
+      },
+      {
+        label: "Office",
+        field: "office",
+        width: 100,
+      },
+      {
+        label: "Age",
+        field: "age",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "Start date",
+        field: "date",
+        sort: "disabled",
+        width: 100,
+      },
+      {
+        label: "Salary",
+        field: "salary",
+        sort: "disabled",
+        width: 100,
+      },
+      {
+        label: "Action",
+        field: "action",
+        width: 100,
+      },
+    ],
+    rows: [
+      {
+        name: "Tiger Nixon",
+        position: "System Architect",
+        office: "Edinburgh",
+        age: "61",
+        date: "2011/04/25",
+        salary: "$320",
+        action: (
+          <div className="justify-content-center text-center">
+            <button className="btn btn-primary fas fa-solid fa-book"></button>
+            <button className="btn btn-danger fa fa-solid fa-trash"></button>
+          </div>
+        ),
+      },
+    ],
+  });
+  return (
+    <>
+      <UserHeader />
+      {/* Page content */}
+
+      <Container className="mt--7" fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <div className="row">
+                  <h3 className="mb-0 col-lg-6">
+                    <strong>Vehicle Details</strong>
+                  </h3>
+                  <div className="col-lg-6 d-flex flex-row-reverse">
+                    <button
+                      className="btn btn-primary p-2"
+                      type="button"
+                      onClick={setModalIsOpenToTrue}
+                    >
+                      Add Vehicle
+                    </button>
+                  </div>
+                </div>
+              </CardHeader>
+              <div style={{ padding: "20px" }}>
+                <MDBDataTableV5
+                  className="detailsTable"
+                  hover
+                  scrollX
+                  entriesOptions={[5, 10, 15]}
+                  entries={5}
+                  pagesAmount={4}
+                  data={datatable}
+                  searchTop
+                  searchBottom={false}
+                />
+              </div>
+            </Card>
+            {/* Modal for add Vehicle */}
+            <div className="addVehicle">
+              <Modal
+                isOpen={modalIsOpen}
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                style={{
+                  overlay: {
+                    justifyContent: "center",
+                    display: "flex",
+                  },
+                  content: {
+                    width: "50%",
+                    minHeight: "25%",
+                    margin: "auto",
+                    backgroundColor: "white",
+                    boxShadow: "5px 4px 20px 20px #0000000f",
+                    padding: "20px",
+                    position: "relative",
+                  },
+                }}
+                className="addVehicle"
+              >
+                <AddVehicle closeAddVehicleModal={setModalIsOpenToFalse} />
+              </Modal>
+            </div>
+          </div>
+        </Row>
+      </Container>
+      <ToastContainer />
+    </>
+  );
+}
+
+Vehicle.layout = Admin;
+
+export default Vehicle;
